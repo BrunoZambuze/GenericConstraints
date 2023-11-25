aed0a734423c0040b7b8a4e34b32b5ec32609c88
BrunoZambuze <zambuzesilva@hotmail.com>
2023-11-25 11:07:32 -0300

    GenericConstraints

---
+++ GenericConstraints/Domain/Product.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace GenericConstraints.Domain
+{
+    internal class Product : IComparable
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public Product()
+        {
+        }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name}, {Price:c}";
+        }
+
+        public int CompareTo(object? obj)
+        {
+            if (!(obj is Product))
+            {
+                throw new ArgumentException("Comparing error: agurment ir not a product");
+            }
+            Product other = obj as Product;
+            return Price.CompareTo(other.Price);
+        }
+    }
+}
---
+++ GenericConstraints/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+using GenericConstraints.Domain;
+using GenericConstraints.Services;
+namespace GenericConstraints
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> list = new List<Product>();
+            Console.Write("Enter N:");
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                string[] vect = Console.ReadLine().Split(',');
+                string name = vect[0];
+                double price = double.Parse(vect[1], CultureInfo.InvariantCulture);
+                list.Add(new Product(name, price));
+            }
+            CalculationService calcularioService = new CalculationService();
+            Product max = calcularioService.Max(list);
+            Console.WriteLine("Max:");
+            Console.WriteLine(max);
+        }
+    }
+}
